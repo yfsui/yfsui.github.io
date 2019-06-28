d642e156158277a80e8e224b75ed0eee7f304dec
Yufeng Sui <anniesui9627@gmail.com>
2019-06-29 00:08:10 +0800

    Change shape

--- src/js/app.js
+++ src/js/app.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
         "value": "#ffffff"
       },
       "shape": {
-        "type": "circle",
+        "type": "star",
         "stroke": {
           "width": 0,
           "color": "#000000"
