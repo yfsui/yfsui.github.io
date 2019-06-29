f8e3c59bd3bc904f1383f67bc51243bb3b8961c8
Yufeng Sui <anniesui9627@gmail.com>
2019-06-29 09:24:56 +0800

    Change particle

--- src/js/app.js
+++ src/js/app.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
   particlesJS('particles-js', {
     "particles": {
       "number": {
-        "value": 160,
+        "value": 100,
         "density": {
           "enable": true,
           "value_area": 500
